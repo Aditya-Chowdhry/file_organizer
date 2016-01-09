f13c6a59cf52c43872a5632939f9c6943ac3e443
Aditya Chowdhry <adityachowdhry09@gmail.com>
2016-01-10 04:13:19 +0530

    Basic reading of files and making directories

--- file_organizer.rb
+++ file_organizer.rb
@@ -1,11 +1,27 @@
 #Get the current directory object
 present_dir=Dir.pwd
 dir=Dir.new(present_dir)
+puts "Enter file extensions you need to organize(seperated by a comma eg. \"doc,pdf,mp3\"):"
+input=gets.chomp()
+file_types=input.split(",")
+puts file_types
+file_types.each do |type|
+  if Dir.exist?(type)
+     Dir.mkdir("#{type.upcase} files")
+  end
+end
 
-#Dir.mkdir(File.join(Dir.home, "aaaaaa"))
+hash_files=Hash.new
 
 dir.each do |file|
-  if File.extname(file)==".rb"
-    puts "#{file}"
+  ext_name=File.extname(file)
+  if hash_files.has_key?(ext_name)
+    hash_files[ext_name]=hash_files[ext_name]+1
+  else
+    hash_files[ext_name]=1
   end
+end
+
+file_types.each do |type|
+  puts "#{type} count: #{hash_files["."+type]}"
 end
